9cd166497553db3861eb96cf33a8b8563cce40e8
frozentape <73592467+frozentape@users.noreply.github.com>
2021-07-22 22:27:12 +0100

    Add railway animation
    Railway is now animated and moves at the same speed as the terrain.

---
+++ Assets/RailsAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailsAnimation : MonoBehaviour
+{
+    public Transform TeleportLocationTransform;
+    public Transform[] Rails;
+
+    private void FixedUpdate()
+    {
+        foreach (var rail in Rails)
+        {
+            // Move the rails at the same rate as the terrain
+            rail.transform.position += new Vector3(0f, 0f, -0.5f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Move the rail to the start once it reaches the trigger
+        other.transform.position = TeleportLocationTransform.position;
+    }
+}
