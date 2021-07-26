c82566f9d320e27e4f07152c316b8cfac5b6f77d
frozentape <73592467+frozentape@users.noreply.github.com>
2021-07-26 21:33:35 +0100

    Add Watcher Nuns
    Adds nuns that turn to watch the train hit the nunnery in the church scene.

--- Assets/TrainPhysics.cs
+++ Assets/TrainPhysics.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,10 +12,14 @@ public class TrainPhysics : MonoBehaviour
     public Transform ExplosionPosition;
     public GameObject[] Nuns;
     public GameObject Church;
+    public static event EventHandler TrainSpawned;
 
     // Start is called before the first frame update
     void Start()
     {
+        TrainSpawned += testFunction;
+        TrainSpawned(this, EventArgs.Empty);
+
         rb = GetComponent<Rigidbody>();
         rb.AddForce(new Vector3(0f, 0f, ImpulseForce), ForceMode.Impulse);
 
@@ -39,8 +44,12 @@ public class TrainPhysics : MonoBehaviour
         Debug.Log("Explode");
         foreach (var nun in Nuns)
         {
-            // Explode in a random direction byt varying the point of the explosion slightly
-            nun.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, nun.transform.position + new Vector3(Random.Range(-2f, 2f), -0.5f, Random.Range(-2f, 2f)), ExplosionRadius);
+            // Don't want to explode the watcher nuns
+            if (nun.GetComponent<WatcherNun>() == null)
+            {
+                // Explode in a random direction byt varying the point of the explosion slightly
+                nun.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, nun.transform.position + new Vector3(UnityEngine.Random.Range(-2f, 2f), -0.5f, UnityEngine.Random.Range(-2f, 2f)), ExplosionRadius);
+            }            
         }
 
         MeshCollider[] churchMeshColliders = Church.GetComponentsInChildren<MeshCollider>();
@@ -52,12 +61,8 @@ public class TrainPhysics : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void testFunction(object sender, EventArgs e)
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    rb.AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius);
-        //}
+        Debug.Log("Train Spawned.");
     }
 }
---
+++ Assets/WatcherNun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatcherNun : MonoBehaviour
+{
+    public Transform Target;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrainPhysics.TrainSpawned += assignTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetDirection = Target.position - transform.position;
+
+        float singleStep = Speed * Time.deltaTime;
+
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    void assignTarget(object sender, EventArgs e)
+    {
+        Target = FindObjectOfType<TrainPhysics>().transform;
+    }
+}
